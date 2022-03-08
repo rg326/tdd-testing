8815157046b1da8ac5a9a8053efa15426e22f69f
Lauren <rg326@live.com>
2022-03-08 12:23:18 -0500

    added time and space comp to linear search

--- python_dsa/py_algos/search_sorting/prob_1.py
+++ python_dsa/py_algos/search_sorting/prob_1.py
@@ -1,4 +1,5 @@
 # # #CARD PROBLEM - LINEAR SEARCH
+##Time: O(N) Space: O(1)
 # # # Alice has some cards with numbers written on them. She arranges the cards in decreasing order, and lays them out face down in a sequence on a table. She challenges Bob to pick out the card containing a given number by turning over as few cards as possible. Write a function to help Bob locate the card.
 
 # STEPS:
