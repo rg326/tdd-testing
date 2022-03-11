0d51868745ccd5897939b2e79be3e8b9077163fc
Lauren <rg326@live.com>
2022-03-11 14:51:14 -0500

    added full example of linear search from treehouse

---
+++ python_dsa/py_algos/search_sorting/linear_search_ex.py
@@ -0,0 +1,33 @@
+# Treehouse Linear Search
+# t = target value (target to be found)
+
+# Must return a val
+# Must complete execution in a finite amount of time
+# Must output the same result everytime for a given input set
+
+def linear_search(list, t):
+	'''
+	Returns the index position of the target if found, else returns None
+	'''
+	
+	for i in range(0, len(list)):
+		if list[i] == t:
+			return i
+	return None
+		
+def verify(index):
+	if index is not None:
+		print('Target found at index: ', index)
+	else:
+		print('Target not found in list')
+		
+numbers = [1,2,3,4,5,6,7,8,9,10]
+	
+result = linear_search(numbers, 12)
+verify(result)
+result = linear_search(numbers, 6)
+verify(result)
+result = linear_search(numbers, 5)
+verify(result)
+	
+		
