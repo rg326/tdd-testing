40267fa57b93f801def05ca7376e005bec421ca2
Lauren <rg326@live.com>
2022-03-12 21:39:55 -0500

    added notes on dictionaries

---
+++ python_dsa/py_ds/linkedin_dictionaries_1.py
@@ -0,0 +1,32 @@
+# Unordered
+# Immutable
+# mapping of unique keys to values
+
+# Python 3.6 and up, goes by insertion order
+
+# Basic Operations
+# - Creating an empty dictionary d = {}
+# - Add an entry d["IV"] = 4
+# - Add three items in the dictionary d = {"I":1, "II":2, "III": 3}
+# - Delete an entry del d["IV"]
+
+'''
+sal_info = {'Austin':91185,'Boston':90171}
+sal_info['Boston'] = 95123
+sal_info['Atlanta'] = 91234
+print(sal_info)
+len(sal_info)
+del sal_info['Atlanta']
+print(sal_info)
+len(sal_info)
+print(sal_info['Atlanta'])
+sal_info.clear()
+print(sal_info)
+len()
+'''
+
+sal_info = dict()
+print(sal_info)
+
+#Populate dictionary
+sal_info = {'Austin':911985,'Dallas':89999,'San Jose':100989,'Atlanta':89286}
