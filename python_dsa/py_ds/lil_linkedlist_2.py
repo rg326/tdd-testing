37bf0a8da85305d844476c01f61515e28bfb4f00
Lauren <rg326@live.com>
2022-03-17 19:20:53 -0400

    set up structure of python linked list

---
+++ python_dsa/py_ds/lil_linkedlist_2.py
@@ -0,0 +1,19 @@
+# Implementation Linked List - LinkedIn Learning
+
+class SLLNode: 
+	
+	def __innit__(self, data):
+		self.data = data
+		self.next = None
+		
+	def get_data(self):
+		pass
+		
+	def set_data(self):
+		pass
+		
+	def get_next(self):
+		pass
+		
+	def set_next(self, new_next):
+		pass
