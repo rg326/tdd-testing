6fc8ee5742c61395b8b847a3c2d99612c8e08a22
Lauren <rg326@live.com>
2022-03-18 17:57:39 -0400

    added doubly linked lists to linkedin learning

--- python_dsa/py_ds/lil_linkedlist_3.py
+++ python_dsa/py_ds/lil_linkedlist_3.py
@@ -1,10 +1,11 @@
 # Implementation Linked List - LinkedIn Learning
 
-class SLLNode: # Single Linked List (SLL) 
+class DLLNode: #Doubly Linked List (DLL)
 	
 	def __init__(self, data):
 		self.data = data
 		self.next = None
+		self.previous = None
 		
 	def __repr__(self):
 		return 'SLLNode object: data={}'.format(self.data)
@@ -25,15 +26,33 @@ class SLLNode: # Single Linked List (SLL)
 		'''Replace the existing value of the self.next attribute with new_next parameter.'''
 		self.next = new_next
 		
-node = SLLNode('apple')
+	def get_previous(self):
+		'''Return the self.previous attribute'''
+		return self.previous
+	
+	def set_previous(self, new_previous):
+		'''Replace the existing value of the self.previous attribute with new_previous parameter.'''
+		self.previous = new_previous
+
+'''		
+node = DLLNode('apple')
 dataget = node.get_data()
 print(dataget)
 
 setdata = node.set_data('7')
 print(setdata)
 
-node2 = SLLNode('carrot') #each is its own list
+node2 = DLLNode('carrot') #each is its own list
 setnext = node.set_next(node2)
 print(setnext)
 getnext = node.get_next()
 print(getnext)
+'''
+# Double linked list needs both a get_next and a get_previous method
+
+node1 = DLLNode(1)
+node2 = DLLNode(2)
+getprev = node1.get_previous()
+print(getprev)
+setprevious = node1.set_previous(node2) 
+print(setprevious)
