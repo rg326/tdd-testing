3c8ee1080c1361175c0bacac1ef86fdbe4031873
Lauren <rg326@live.com>
2022-03-17 22:17:42 -0400

    added methods and test cases to linked list

--- python_dsa/py_ds/lil_linkedlist_2.py
+++ python_dsa/py_ds/lil_linkedlist_2.py
@@ -2,18 +2,38 @@
 
 class SLLNode: 
 	
-	def __innit__(self, data):
+	def __init__(self, data):
 		self.data = data
 		self.next = None
 		
-	def get_data(self):
-		pass
+	def __repr__(self):
+		return 'SLLNode object: data={}'.format(self.data)
 		
-	def set_data(self):
-		pass
+	def get_data(self):
+		'''Return the self.data attribute'''
+		return self.data
+		
+	def set_data(self, new_data):
+		'''Replace the existing value of the self.data attribute with new_data parameter'''
+		self.data = new_data
 		
 	def get_next(self):
-		pass
+		'''Return the self.next attribute'''
+		return self.next
 		
 	def set_next(self, new_next):
-		pass
+		'''Replace the existing value of the self.next attribute with new_next parameter.'''
+		self.next = new_next
+		
+node = SLLNode('apple')
+dataget = node.get_data()
+print(dataget)
+
+setdata = node.set_data('7')
+print(setdata)
+
+node2 = SLLNode('carrot') #each is its own list
+setnext = node.set_next(node2)
+print(setnext)
+getnext = node.get_next()
+print(getnext)
