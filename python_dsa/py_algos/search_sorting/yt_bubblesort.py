4e6f7c0e3a36f3c3d3a81a245093048aa935b13c
Lauren <rg326@live.com>
2022-03-20 20:45:12 -0400

    added correct bubblesort algo

--- python_dsa/py_algos/search_sorting/yt_bubblesort.py
+++ python_dsa/py_algos/search_sorting/yt_bubblesort.py
@@ -19,9 +19,19 @@ def bubblesort(list):
 	return bubblesort(new_list)
 '''
 
+def bubblesort(list):
+	index_length = len(list) - 1
+	sorted = False
 	
+	while not sorted:
+		sorted = True
+		for i in range(0, index_length):
+			if list[i] > list[i+1]:
+				sorted = False
+				list[i], list[i+1] = list[i+1], list[i]	
+	return list
 	
 '''test case'''
-print(bubblesort([1,2,8,4,6,9,3,2]))
+print(bubblesort([1,2,7,8,4,6,9,3,5,2]))
 		
 		
