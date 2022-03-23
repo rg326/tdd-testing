bbcfd5146da4f4fa563b24ce1d8f4047c9f1c786
Lauren <rg326@live.com>
2022-03-23 16:21:29 -0400

    chapter 1 invent python games

---
+++ invent_games/ch_1_shell.py
@@ -0,0 +1,46 @@
+#Expressions
+
+2+2+2+2+2
+8*6
+10-5+6
+2+2
+
+8*3 / 2+2+7-9
+
+#Syntax Errors
+
+5 +
+
+#Storing values in variables
+spam = 15
+spam
+spam + 5
+#20
+
+spam = 15
+spma
+'''Error'''
+spam + 5
+spam = 3
+spam + 5
+#8
+
+spam = 15
+spam = spam + 5
+#20
+
+spam = 15
+spam = spam + 5
+spam = spam + 5
+spam + spam + 5
+spam
+#30
+
+bacon = 10
+eggs = 15
+
+bacon = 10
+eggs = 15
+spam = bacon + eggs
+spam
+#25
