48a432d430ae926d5caf9c7e129e9ed6356dbe43
Lauren <rg326@live.com>
2022-03-24 18:18:46 -0400

    added ch2 of invent python games

---
+++ invent_games/ch2_writing_programs.py
@@ -0,0 +1,12 @@
+#String Values
+spam = 'hello'
+print(spam)
+
+#String Concatenation
+print('Hello ' + 'World')
+
+# This program says hello and asks for my name
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName + '!')
