e8f63231ff806c29cb4cbedd90f08cd249f862f4
Lauren <rg326@live.com>
2022-03-26 17:38:38 -0400

    fixed bugs in number guessing game

--- invent_games/ch3_number_guess.py
+++ invent_games/ch3_number_guess.py
@@ -7,26 +7,26 @@ print('Hello! What is your name?')
 myName = input()
 
 number = random.randint(1, 20)
-	print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 	
-	for guessesTaken in range(6):
-		print('Take a guess.') # Four spaces in front of 'print'
-			guess = input()
-			guess = int(guess)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+	
+for guessesTaken in range(6):
+	print('Take a guess.') # Four spaces in front of 'print'
+	guess = input()
+	guess = int(guess)
 			
-			if guess < number:
-				print('Your guess is too low!')
-				# Eight spaces in front of print
-			elif guess > number:
-				print('Your guess is too high!')
-			elif guess == number:
-				break
+if guess < number:
+	print('Your guess is too low!')
+	# Eight spaces in front of print
+elif guess > number:
+	print('Your guess is too high!')
+elif guess == number: break
 				
-	if guess == number:
-		guessesTaken = str(guessesTaken + 1)
-		print('Good job, ' + myName + '! You guessed the number in ' + guessesTaken + ' guesses!')
+if guess == number:
+	guessesTaken = str(guessesTaken + 1)
+	print('Good job, ' + myName + '! You guessed the number in ' + guessesTaken + ' guesses!')
+	
+if guess != number:
+	number = str(number)
+	print('Nope. The number I was thinking of was ' + number + '.') 
 		
-	if guess != number:
-		number = str(number)
-		print('Nope. The number I was thinking of was ' + number + '.') 
-			
