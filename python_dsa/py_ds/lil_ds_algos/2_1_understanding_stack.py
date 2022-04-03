c5e4dfede9a6c2189d84cb63533d351ab7b721f5
Lauren <rg326@live.com>
2022-04-03 19:02:02 -0400

    added notes for lil stack ds

---
+++ python_dsa/py_ds/lil_ds_algos/2_1_understanding_stack.py
@@ -0,0 +1,13 @@
+#### Understanding Stack
+
+# Reverse Polish notation for evaluating arithmeric expressions
+
+# Syntax parsing
+
+# Cold stack: space for parameters and local variables is created
+
+# Used in recursion
+
+# Undo and redo operations in word processors
+
+# Low-level assembly programming
