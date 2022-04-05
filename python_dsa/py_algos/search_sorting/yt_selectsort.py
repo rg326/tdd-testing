c98d9ec1e1fdebffe8ae7a2a2d58444f43d56e1a
Lauren <rg326@live.com>
2022-04-05 18:46:31 -0400

    added final notes to selection sort

--- python_dsa/py_algos/search_sorting/yt_selectsort.py
+++ python_dsa/py_algos/search_sorting/yt_selectsort.py
@@ -4,14 +4,15 @@ def selection_sort(list):
 	index_length = range(0, len(list)-1)
 	
 	for i in index_length:
-		min_val = i
+		min_val = i #set starting val
 		
 		for j in range(i+1, len(list)):
-			if list[j] < list[min_val]:
-				min_val = j
+			if list[j] < list[min_val]: #compare
+				min_val = j #replace min val
 			
-		if min_val != i:
+		if min_val != i: #if new val lower than min_val
 			list[min_val], list[i] = list[i], list[min_val]
+			# switch min vals
 			
 	return list
 		
