c33f4594e9218562a13e7b45d31f43c6b3208c6b
Lauren <rg326@live.com>
2022-04-04 21:13:19 -0400

    finished building stack

--- python_dsa/py_ds/lil_ds_algos/2_2_building_stack.py
+++ python_dsa/py_ds/lil_ds_algos/2_2_building_stack.py
@@ -20,3 +20,27 @@ class Stack:
 	def isEmpty(self):
 		# return len(self.items) == 0
 		return not self.items
+		
+	def push(self, item):
+		self.items.append(item)
+		
+	def pop(self):
+		self.items.pop()
+		
+	def peek(self):
+		return self.items[-1]
+		
+	def size(self):
+		return len(self.items)
+		
+	def __str__(self):
+		return str(self.items)
+		
+if __name__ == '__main__':
+	s = Stack()
+	print(s)
+	print(s.isEmpty())
+	s.push(3)
+	
+		
+		
