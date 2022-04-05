0a8592e9ab71560482d9001440987ae4bfa8df58
Lauren <rg326@live.com>
2022-04-05 18:46:31 -0400

    fixed bugs and added test case and notes for selection sort

--- python_dsa/py_algos/search_sorting/yt_selectsort.py
+++ python_dsa/py_algos/search_sorting/yt_selectsort.py
@@ -7,12 +7,13 @@ def selection_sort(list):
 		min_val = i
 		
 		for j in range(i+1, len(list)):
-			min_val = j
+			if list[j] < list[min_val]:
+				min_val = j
 			
-		if min_value not i:
+		if min_val != i:
 			list[min_val], list[i] = list[i], list[min_val]
 			
-		return list
+	return list
 		
-
+print(selection_sort([3,8,4,8,5,6,4,8,9,7,4,1,2,4]))
 		
