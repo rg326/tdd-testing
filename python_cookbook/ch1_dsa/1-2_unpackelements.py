2c885bfc4c8cfc26e157a53de4ee539a3227d9b6
Lauren <rg326@live.com>
2022-04-08 20:52:16 -0400

    added extra notes to ex 1.2 of cookbook

--- python_cookbook/ch1_dsa/1-2_unpackelements.py
+++ python_cookbook/ch1_dsa/1-2_unpackelements.py
@@ -2,7 +2,7 @@
 
 # Problem: You need to unpack N-elements form an iterable, but the iterable may be longer than N elements, causing a "too many values to pack" exception.
 
-# Solution: Python "star expressions" can be used to address this problem.
+# Solution: Python "star expressions" can be used to address this problem. 
 
 def drop_first_last(grades):
 	first, *middle, last = grades
@@ -15,3 +15,5 @@ name, email, *phone_numbers = user_record
 print(name)
 print(email)
 print(phone_numbers)
+
+# Note: The phone_numbers variable will always be a list, regardless of how many phone numbers are unpacked(including none). Thus, any code that uses phone_numbers won't have to account for the possibility that it might not be a list or perform any kind of additional type checking.
