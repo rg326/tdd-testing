c1509289ee46bd66d08a5eb2eab7462be47b8999
Lauren <rg326@live.com>
2022-04-09 18:46:40 -0400

    added binary tree to python dsa

---
+++ python_dsa/py_ds/yt_binarytree_bfs.py
@@ -0,0 +1,26 @@
+# Binary Tree and Breadth First Search (Algorithm)
+
+# Tree = 'gcbaedfihjk'
+
+# Class BTree = Tree Node construction
+
+class BTree:
+	def __init__(self, data):
+		self.data = data
+		self.left = None
+		self.right = None
+		
+	def insert(self, data):
+		if self.data is None:
+			self.data = data
+		else:
+			if data < self.data:
+				if self.left is None:
+					self.left = TreeNode(data)
+				else:
+					self.left.insert(data)
+			elif data > self.data:
+				if self.right is None:
+					self.right = TreeNode(data)
+				else:
+					self.right.insert(data)
