cef3f4882a5cc71862f6cf997d181ac4398f642d
Lauren <rg326@live.com>
2022-04-11 18:38:59 -0400

    added h-index algo from epi

---
+++ python_dsa/epi/1-h_index.py
@@ -0,0 +1,23 @@
+# Create an algo to find the h-index of a researcher's publications
+
+def h_index(list):
+	h = len(list)
+	list.sort(reverse = True)
+	count = 0
+	
+	for n in list:
+		if n >= h:
+			count += 1
+			
+	return count
+
+print(h_index([2,7,4,8]))
+
+
+
+def addition(num_a, num_b):
+	print(num_a + num_b)
+	
+	
+addition(3, 5)
+		
