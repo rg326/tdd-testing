69a5ad22f90fc1dbf64305536a3c5083786a93f1
Lauren <rg326@live.com>
2022-04-12 20:02:38 -0400

    added bubblesort practice

---
+++ python_dsa/py_algos/search_sorting/bubblesort_attempt1.py
@@ -0,0 +1,16 @@
+# BubbleSort Practice
+
+def bubblesort(list):
+	length = len(list)-1
+	
+	for i in range(0, length):
+		for j in range(length):
+			if(list[j] > list[j+1]):
+				temp_storage = list[j]
+				list[j] = list[j+1]
+				list[j+1] = temp_storage
+	return list
+		
+		
+print(bubblesort([7,3,6,3,4,6,5,6]))
+		
