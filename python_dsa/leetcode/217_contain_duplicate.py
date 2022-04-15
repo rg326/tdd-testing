689e7dcd5f58f6f3eabf5298b0eec6d00a329d27
Lauren <rg326@live.com>
2022-04-14 21:19:43 -0400

    added leetcode 217 contains duplicates

---
+++ python_dsa/leetcode/217_contain_duplicate.py
@@ -0,0 +1,11 @@
+#217 - Contains Duplicate
+
+class Solution(object):
+	def containsDuplicate(self, nums):
+		hashset = set()
+
+		for n in nums:
+			if n in hashset:
+				return True
+			hashset.add(n)
+	return False
