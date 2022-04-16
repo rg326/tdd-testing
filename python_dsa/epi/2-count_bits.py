c72de13a0e4839330c4e541edc32f6740b2af4f7
Lauren <rg326@live.com>
2022-04-16 18:14:35 -0400

    added additional notes to count bits

--- python_dsa/epi/2-count_bits.py
+++ python_dsa/epi/2-count_bits.py
@@ -1,6 +1,10 @@
+# Chapter 4 1.1
+
 def count_bits(x: int) -> int:
 	num_bits = 0
 	while x:
 		num_bits += x & 1
 		x >>= 1
 	return num_bits
+	
+	# https://realpython.com/python-bitwise-operators/
