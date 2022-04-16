d3b075d6020ae2c00e7dc20290a40ad541485d3a
Lauren <rg326@live.com>
2022-04-15 21:17:22 -0400

    added another binary search example

--- python_dsa/py_algos/search_sorting/yt_binary_search.py
+++ python_dsa/py_algos/search_sorting/yt_binary_search.py
@@ -1,4 +1,4 @@
-# Binary Search Algorithm O(nlogn)
+# Binary Search Algorithm O(nlogn) - List Must Be Sorted
 
 def binary_search(list, t):
 	low = 0
@@ -15,4 +15,9 @@ def binary_search(list, t):
 			low = mid + 1 # change list	to start 1 pos above mid
 			
 	return None
-		
+
+
+list_test = [1,2,3,4,5,6,7,8,9,10]
+
+print(binary_search(list_test, 4))
+print(binary_search(list_test, 11))
