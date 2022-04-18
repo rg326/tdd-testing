e389543ca6230532073e3480dcb5704c1855128e
Lauren <rg326@live.com>
2022-04-17 22:33:15 -0400

    added new game tut folder

---
+++ youtube/5_games/start.py
@@ -0,0 +1,23 @@
+while True:
+	txt = """Mini Games!!!
+		- Guess The Number (1)
+		- Rock, Paper, Scissors (2)
+		- Wordle (3)
+		- Connect Four (4)
+		- Tic Tac Toe (5)
+	
+	Select a game (press a number or 'q' to quit): """
+	val = input(txt)
+	
+	if val == '1':
+		pass
+	elif val == '2':
+		pass
+	elif val == '3':
+		pass
+	elif val == '4':
+		pass
+	elif val == '5':
+		pass 
+	elif val == 'q': #so it doesnt need to be a variable, just input
+		break
