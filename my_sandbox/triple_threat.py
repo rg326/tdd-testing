e562e4639b536fe98e77697d924ea694961df0e4
Lauren <rg326@live.com>
2022-05-12 20:20:39 -0400

    added notes for triple threats

--- my_sandbox/triple_threat.py
+++ my_sandbox/triple_threat.py
@@ -10,10 +10,30 @@
 
 # Build Notes
 # Attempt to store deck using a dictionary instead or an array, in order to assign specific values to each type of card
+# Added test code to work with alt solution
 
-import random
 
 def triple_threat():
+	card_deck = {
+		
+		
+	}
+	
+	
+	
+	
+triple_threat()
+
+
+''' import random
+card_points =['A','K','Q','J','2','3','4','5','6','7','8','9','10']
+card_signs =['Heart','CLUB','DIAMOND','SPADE']
+random_point = random.choice(card_points)
+random_sign = random.choice(card_signs)
+random_card = random_point,random_sign
+print(random_card) '''
+
+'''def triple_threat():
 	card_deck = ['1h','1d','1s','1c','2h','2d','2s','2c','3h','3d','3s','3c','4h','4d','4s','4c','5h','5d','5s','5c','6h','6d','6s','6c','7h','7d','7s','7c','8h','8d','8s','8c','9h','9d','9s','9c','10h','10d','10s','10c','Jh','Jd','Js','Jc','Qh','Qd','Qs','Qc','Kh','Kd','Ks','Kc','Jo1','Jo2']
 	my_turn = random.sample(card_deck, 3)
 	computer = random.sample(card_deck, 3)
@@ -24,7 +44,7 @@ def triple_threat():
 			
 					
 									
-triple_threat()
+triple_threat()'''
 
 
 
