cfe4232fb897d52f39d050a4d789a61d646912e4
Lauren <rg326@live.com>
2022-11-17 22:11:11 -0500

    added ex2 to ch1

--- lpthw/ch1.py
+++ lpthw/ch1.py
@@ -1,4 +1,4 @@
-#Ex1
+#Ex1 - Print Statements
 print('Hello World')
 print('Hello Again.')
 print('I like typing this.')
@@ -6,3 +6,16 @@ print('This is fun.')
 print('Yay! Printing.')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+
+#Ex2 - Commenting
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment after is ignored.
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+
