feb59c660a3306527ebbd1f11c419dd89d050454
Lauren <rg326@live.com>
2022-12-06 22:02:58 -0500

    added notes, ttv2

--- my_sandbox/triple_threatv2.py
+++ my_sandbox/triple_threatv2.py
@@ -56,6 +56,7 @@
 # attempt to separate create deck function from the actual deck
 # accidentally reverted changes -_-;. try again tomorrow
 # attempt to create and initiate starting deck variable in start game function instead
+# attempt to ensure deck is recreated upon restart game. might create a restart game function
 
 # ======
 
