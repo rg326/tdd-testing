fbaf320e607eadd3945e64407294ba1e60f808e0
Lauren <rg326@live.com>
2022-12-10 23:37:47 -0500

    small edit, ttv2

--- my_sandbox/triple_threatv2.py
+++ my_sandbox/triple_threatv2.py
@@ -58,7 +58,8 @@
 # attempt to create and initiate starting deck variable in start game function instead
 # attempt to ensure deck is recreated upon restart game. might create a restart game function
 # breaks out of game loop, try keeping restart_game() from resetting game altogether
-#fixed game from resetting but back to population bug again
+# fixed game from resetting but back to population bug again
+# attempt to add deck to restart game function
 
 # ======
 
