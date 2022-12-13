941e5eaa7bd3ad89c63665f9937930fc76dcf01b
Lauren Morgan <rg326@live.com>
2022-12-12 22:28:11 -0500

    removed restore deck, ttv2

--- my_sandbox/triple_threatv2.py
+++ my_sandbox/triple_threatv2.py
@@ -274,70 +274,8 @@ def restart_game():
 	card_deck = copy.deepcopy(starting_deck)
 	start_game()
 	#return starting_deck
+		
 
-
-# function to restore deck on game replay			
-'''def restore_deck():
-	global starting_deck
-	
-	card_deck = {
-	'A of Hearts': 1,
-	'A of Diamonds': 1,
-	'A of Clubs': 1,
-	'A of Spades': 1,
-	'2 of Hearts': 2,
-	'2 of Diamonds': 2,
-	'2 of Clubs': 2,
-	'2 of Spades': 2,
-	'3 of Hearts': 3,
-	'3 of Diamonds': 3,
-	'3 of Clubs': 3,
-	'3 of Spades': 3,
-	'4 of Hearts': 4,
-	'4 of Diamonds': 4,
-	'4 of Clubs': 4,
-	'4 of Spades': 4,
-	'5 of Hearts': 5,
-	'5 of Diamonds': 5,
-	'5 of Clubs': 5,
-	'5 of Spades': 5,
-	'6 of Hearts': 6,
-	'6 of Diamonds': 6,
-	'6 of Clubs': 6,
-	'6 of Spades': 6,
-	'7 of Hearts': 7,
-	'7 of Diamonds': 7,
-	'7 of Clubs': 7,
-	'7 of Spades': 7,
-	'8 of Hearts': 8,
-	'8 of Diamonds': 8,
-	'8 of Clubs': 8,
-	'8 of Spades': 8,
-	'9 of Hearts': 9,
-	'9 of Diamonds': 9,
-	'9 of Clubs': 9,
-	'9 of Spades': 9,
-	'10 of Hearts': 10,
-	'10 of Diamonds': 10,
-	'10 of Clubs': 10,
-	'10 of Spades': 10,
-	'Jack of Hearts': 3,
-	'Jack of Diamonds': 3,
-	'Jack of Clubs': 3,
-	'Jack of Spades': 3,
-	'Queen of Hearts': 3,
-	'Queen of Diamonds': 3,
-	'Queen of Clubs': 3,
-	'Queen of Spades': 3,
-	'King of Hearts': 3,
-	'King of Diamonds': 3,
-	'King of Clubs': 3,
-	'King of Spades': 3,
-	'Joker A': 10,
-	'Joker B': 10
-	}
-	
-	card_deck = copy.deepcopy(starting_deck)'''
 
 # function to end the game
 def end_game():
