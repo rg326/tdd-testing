f1d40a59a9f0179ae6da6a7b2d4dd82d1babb3f3
Lauren Morgan <rg326@live.com>
2022-12-14 22:20:20 -0500

    added notes to kokugojiten.py

--- webscraping/kokugojiten.py
+++ webscraping/kokugojiten.py
@@ -8,3 +8,7 @@ url = ''
 page = requests.url('')
 
 soup = BeautifulSoup(url.text, 'html.parser')
+
+#Use soup.find to find data
+
+#Project: Webscrape entries from dictionary and transfer to CSV files
