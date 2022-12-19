21cc2190ee86ba78761fd92285ff354eacb6a4ca
Lauren Morgan <rg326@live.com>
2022-12-18 20:44:05 -0500

    name fix, etc, ttv3

---
+++ my_sandbox/triple_threatv3.py
@@ -0,0 +1,32 @@
+#NOTES
+
+
+
+
+# =========
+
+# Import modules
+import random
+import copy
+import time
+
+#Variables
+
+starting_deck = {}
+
+starting_deck = create_deck()
+#COPYING STARTING DECK BY VALUE TO EMPTY CARD DECK
+#card_deck = {}
+card_deck = copy.deepcopy(starting_deck)
+
+#Getting the keys of the deck as an array for each hand played
+hand = card_deck.keys()
+
+def play_turn():
+	global hand
+	global my_score
+	global comp_score
+	global my_hand
+	global comp_hand
+	global starting_deck
+	global card_deck
