2759b31beb7b0bc7f247810b19fc124d43a9d37e
Lauren <rg326@live.com>
2022-12-29 22:14:58 -0500

    added notes, ttv3

--- my_sandbox/triple_threatv3.py
+++ my_sandbox/triple_threatv3.py
@@ -3,7 +3,7 @@
 # Still stuck with bug. Will return to fixing upon end of break.
 # Start with continuing to refactor the code from previous version.
 #Check out tutorial for deepcopy
-
+#Fix the population bug for 2023
 # =========
 
 # Import modules
