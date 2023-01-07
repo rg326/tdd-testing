67c4ea52c8ec5800e228ffb4758079246f1c3452
Lauren <rg326@live.com>
2023-01-06 22:57:42 -0500

    added notes to variable file, a_s

--- ardit_sulce/mega_course/sec3_variables.py
+++ ardit_sulce/mega_course/sec3_variables.py
@@ -1,3 +1,6 @@
+# 変数
+# better to have spaces around the operator for better readability
+
 spent = 3
 
 donated = 4
@@ -6,3 +9,9 @@ total_amount = spent + donated
 
 print(total_amount)
 
+items = 10
+price = 2
+
+total_price = items * price
+
+print(total_price)
