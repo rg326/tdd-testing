8bf268031687341a5ae48d51feaa5293df811876
Lauren <rg326@live.com>
2023-01-11 21:33:38 -0500

    added mean to datatypes.py, a_s

--- ardit_sulce/mega_course/sec3_datatypes.py
+++ ardit_sulce/mega_course/sec3_datatypes.py
@@ -4,3 +4,11 @@ greeting = "hello world"
 
 #Upper
 str.upper(greeting)
+
+#Calculate AVG
+#dir(list) directory for list methods
+
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
