614ce49c8210ba6606ec3bc8c588ae539519e745
Lauren <rg326@live.com>
2023-01-13 23:22:35 -0500

    minor edit, py_tricks

--- python_tricks/ch_2_patterns.py
+++ python_tricks/ch_2_patterns.py
@@ -4,3 +4,4 @@
 def apply_discount(product, discount):
 price = int(product['price'] * (1.0 - discount)) assert 0 <= price <= product['price']
 return price
+>>> shoes = {'name': 'Fancy Shoes', 'price': 14900}
