5df351e63e0a81bddd63660133cb4769aebf5979
Lauren <rg326@live.com>
2023-01-20 21:48:51 -0500

    added append ex to listops, a_s

--- ardit_sulce/mega_course/sec4_listops.py
+++ ardit_sulce/mega_course/sec4_listops.py
@@ -5,3 +5,4 @@ monday_temps = [9.1, 8.8, 7.5]
 monday_temps.append(8.1)
 
 #First parameter of the append method is actually "self", which refers to the class list of which append is a part of.
+monday_temps = (9.6)
