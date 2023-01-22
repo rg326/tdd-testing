d71d11c948039aa031d775f420b0dcb9fdfbb3d6
Lauren <rg326@live.com>
2023-01-21 21:27:42 -0500

    added more list operations sec4, a_s

--- ardit_sulce/mega_course/sec4_listops.py
+++ ardit_sulce/mega_course/sec4_listops.py
@@ -6,3 +6,9 @@ monday_temps.append(8.1)
 
 #First parameter of the append method is actually "self", which refers to the class list of which append is a part of.
 monday_temps = (9.6)
+
+#Index
+monday_temps.index(8.8)
+
+#Clear
+monday_temps.clear()
