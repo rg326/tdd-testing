3d92ee13b12f715776ed1278ef869d4d89c61dfb
Lauren <rg326@live.com>
2023-01-21 21:27:42 -0500

    added defs to list operations sec, a_s

--- ardit_sulce/mega_course/sec4_listops.py
+++ ardit_sulce/mega_course/sec4_listops.py
@@ -1,14 +1,20 @@
 monday_temps = [9.1, 8.8, 7.5]
 
-#Append
+#Append - Append object to the end of the list (push)
 
 monday_temps.append(8.1)
+
+#append(self, object, /)
 
 #First parameter of the append method is actually "self", which refers to the class list of which append is a part of.
 monday_temps = (9.6)
 
-#Index
+#Index - Return first index of value. Raises ValueError if the value is not present.
 monday_temps.index(8.8)
 
-#Clear
+#index(self, value, start = 0, stop = 0)
+
+#Clear - Remove all items from the list
 monday_temps.clear()
+
+#clear(self, /)
