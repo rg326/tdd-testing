c08266d816a7a00e56b6ffb2f0a1b0e3547f9d83
Lauren <rg326@live.com>
2023-01-30 23:17:33 -0500

    added notes dictionaries.py sec4, a_s

---
+++ ardit_sulce/mega_course/sec4_dictionaries.py
@@ -0,0 +1,7 @@
+#In this lecture, you will learn more about dictionaries, so here's an example of a dictionary in case you forgot about them:
+
+search_engines_users = {"google": 1000000000, "bing": 127000000, "duck duck go":12000000}
+
+#A dictionary is made of pairs of keys and values. For example, the first pair is "google": 1000000000 where "google" is the key and 1000000000 is the value of that key.
+
+#Let's go!
