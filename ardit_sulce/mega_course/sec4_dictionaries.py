017108a3e47c8b71e505961f4f019d66e0a78b10
Lauren <rg326@live.com>
2023-01-31 23:23:56 -0500

    added new dict sec4, a_s

--- ardit_sulce/mega_course/sec4_dictionaries.py
+++ ardit_sulce/mega_course/sec4_dictionaries.py
@@ -5,3 +5,5 @@ search_engines_users = {"google": 1000000000, "bing": 127000000, "duck duck go":
 #A dictionary is made of pairs of keys and values. For example, the first pair is "google": 1000000000 where "google" is the key and 1000000000 is the value of that key.
 
 #Let's go!
+
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
