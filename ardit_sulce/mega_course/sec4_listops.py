c01416afeb513d7741d517c9be84dc1b0a4efdda
Lauren <rg326@live.com>
2023-02-03 22:41:58 -0500

    minor edit sec4, a_s

--- ardit_sulce/mega_course/sec4_listops.py
+++ ardit_sulce/mega_course/sec4_listops.py
@@ -1,6 +1,7 @@
-x# List Operations and Atrributes
+# List Operations and Atrributes
 
-monday_temps = [9.1, 8.8, 7.5]
+#monday_temps = [9.1, 8.8, 7.5]
+monday_temps = [9, 8, 7, 4, 5]
 
 #Append - Append object to the end of the list (push)
 
@@ -23,6 +24,7 @@ monday_temps.clear()
 
 # Get item with index #
 monday_temps.__getitem__(1)
+
 # Alt
 monday_temps[1]
 
