3d830a8d311f43ec66e92bdc4f7e9bdaa0a0c467
Lauren <rg326@live.com>
2023-02-07 22:46:37 -0500

    added list conditionals sec5, a_s

--- ardit_sulce/mega_course/sec5_conditionals.py
+++ ardit_sulce/mega_course/sec5_conditionals.py
@@ -7,6 +7,7 @@ def mean(val):
         the_mean = sum(myList) / len(myList)
         return the_mean
 
+monday_temperatures = [8.8, 9.1, 9.9]
 student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
 
 print(mean(student_grades))
