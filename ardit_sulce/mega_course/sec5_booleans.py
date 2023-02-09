0dd3ebe44d468d9a0277cb2c1b7b5e42b47ec969
Lauren <rg326@live.com>
2023-02-08 22:44:28 -0500

    added or operator booleans sec5, a_s

--- ardit_sulce/mega_course/sec5_booleans.py
+++ ardit_sulce/mega_course/sec5_booleans.py
@@ -16,3 +16,15 @@ else:
     print("No")
 
 # This will return Yes since x == 1 and y == 1 are both True
+
+# You can also check if one or two conditions are met using an or operator
+
+x = 1
+y = 1
+
+if x == 1 or y == 2:
+    print("Yes")
+else:
+    print("No")
+
+# That will return Yes since at least one of the conditions is True. In this case, x == 1 is True.
