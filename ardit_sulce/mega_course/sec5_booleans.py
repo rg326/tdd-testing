6a44b745ebfe4ae98400f1d1b95e02f423320c57
Lauren <rg326@live.com>
2023-02-08 22:44:28 -0500

    added example of and operator booleans sec5, a_s

---
+++ ardit_sulce/mega_course/sec5_booleans.py
@@ -0,0 +1,18 @@
+# AND and OR operators
+
+x = 1
+if x == 1:
+    print("Yes")
+else:
+    print("No")
+
+# You can check if two conditions ar met at the same time using an and operator
+x = 1
+y = 1
+
+if x == 1 and y == 1:
+    print("Yes")
+else:
+    print("No")
+
+# This will return Yes since x == 1 and y == 1 are both True
