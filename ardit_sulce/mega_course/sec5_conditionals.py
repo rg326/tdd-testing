c59de1f46e9cfbd51ad5a6db05f41a38066529d0
Lauren <rg326@live.com>
2023-02-12 22:40:33 -0500

    added elif conditionals sec5, a_s

--- ardit_sulce/mega_course/sec5_conditionals.py
+++ ardit_sulce/mega_course/sec5_conditionals.py
@@ -11,5 +11,15 @@ monday_temperatures = [8.8, 9.1, 9.9]
 student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
 
 print(mean(student_grades))
-
+j
 #Allows to control for different avenues or branches of choices
+
+# Elif conditionals
+x = 3
+y = 1
+if x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+else:
+    print("x is less than y")
