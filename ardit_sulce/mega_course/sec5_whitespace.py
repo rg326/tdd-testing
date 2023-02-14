69de3362dec5480243907c939fabf3b74aa4077a
Lauren <rg326@live.com>
2023-02-13 21:04:25 -0500

    added notes to whitespace sec5, a_s

--- ardit_sulce/mega_course/sec5_whitespace.py
+++ ardit_sulce/mega_course/sec5_whitespace.py
@@ -1,14 +1,22 @@
-if 3 > 1:
+#Whitespace Convention
+
+# Too many spaces (6 here) not readable
+if      3 > 1:
     print('a')
 
 print('aa')
 print('aaa')
 
+# Space needed between operators (3 > 1)
 if 3>1:
     print('b')
 
 print('bb')
 print('bbb')
+
+# Correct convention
+if 3 > 1:
+    print('c')
 
 print('cc')
 print('ccc')
