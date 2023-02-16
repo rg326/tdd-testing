c40bd24e434005d9ce122673a8dfc1d789e200aa
Lauren <rg326@live.com>
2023-02-16 15:42:29 -0500

    added user input example sec6, a_s

---
+++ ardit_sulce/mega_course/sec6_userinput.py
@@ -0,0 +1,10 @@
+def weather_condition(temp):
+    if temp > 7:
+        return "Warm"
+    else:
+        return "Cold"
+
+user_input = int(input("Enter some input:"))
+
+print(type(user_input), user_input)
+    
