39e5753be33426fe0a12c611701ff245952a625a
Lauren <rg326@live.com>
2023-02-18 23:04:13 -0500

    added multi str formatting notes sec6, a_s

--- ardit_sulce/mega_course/sec6_stringformat.py
+++ ardit_sulce/mega_course/sec6_stringformat.py
@@ -4,3 +4,6 @@ user_input = input("Enter your name: ")
 message = "Hello %s!" % user_input #Python 2.3
 message = f"Hello {user_input}" #Python 3^
 print(message)
+
+#Multiple strings/variable inputs for Python 2.3
+message = "Hello %s" % (name, surname)
