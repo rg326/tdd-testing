682848d2640fcfbb671f49b732bece4d932919f7
Lauren <rg326@live.com>
2023-02-18 23:04:13 -0500

    added notes to str formatting sec6, a_s

--- ardit_sulce/mega_course/sec6_stringformat.py
+++ ardit_sulce/mega_course/sec6_stringformat.py
@@ -1,5 +1,6 @@
 # String Formatting
 
 user_input = input("Enter your name: ")
-message = "Hello %s!" % user_input
+message = "Hello %s!" % user_input #Python 2.3
+message = f"Hello {user_input}" #Python 3^
 print(message)
