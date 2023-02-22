6981d3290cdfa1b15f5bfdb0a938a4abb58eb2fb
Lauren <rg326@live.com>
2023-02-21 22:26:22 -0500

    added final notes user input and format strings sec6, a_s

--- ardit_sulce/mega_course/sec6_userinput.py
+++ ardit_sulce/mega_course/sec6_userinput.py
@@ -10,4 +10,19 @@ print(type(user_input), user_input)
 
  #Final Notes
  # In this section you learned that:
- #    
+ # A python program can get user input via the input function
+ # The input function halts the eecution of the program and gets text input from the user
+
+ name = input("Enter your name: ")
+
+ # The input function converts any input to a string, but you can convert it back to int or float:
+
+ experience_months = input("Enter your experience in months: ")
+ experience_years = int(experience_months) /12
+
+ # You can also format strings with:
+
+ name = "Sim"
+ experience_years = 1.5
+ print("Hi {}, you have {} years of experience").format(name, experience_years))
+ # Output: Hi Sim, you have 1.5 years of experience. 
