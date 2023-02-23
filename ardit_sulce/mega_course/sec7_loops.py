c046516ad634a7b8087dac43c7a1845743e41440
Lauren <rg326@live.com>
2023-02-22 21:32:25 -0500

    added examples loops sec7, a_s

---
+++ ardit_sulce/mega_course/sec7_loops.py
@@ -0,0 +1,10 @@
+monday_temp = [9.1, 8.8, 7.6]
+
+print(round(monday_temp[0])) #x3
+
+#For loops automatically reiterate over commands
+for temp in monday_temp:
+    print(round(temp)) 
+
+for letter in "hello":
+    print(letter.title())
