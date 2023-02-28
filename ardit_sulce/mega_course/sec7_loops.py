533864139f1ed0fbf5cfd7616674df40bc58e092
Lauren <rg326@live.com>
2023-02-27 22:48:50 -0500

    added notes to improve prev example sec7, a_s

--- ardit_sulce/mega_course/sec7_loops.py
+++ ardit_sulce/mega_course/sec7_loops.py
@@ -77,3 +77,9 @@ phone_numbers = {"John": "+37682929928", "Marry": "+423998200919"}
      
     for pair in phone_numbers.items():
         print(f"{pair[0]} has as phone number {pair[1]}")
+
+# A better way to achieve the same results by iterating keys and values
+phone_numbers = {"John": "+37682929928", "Marry": "+423998200919"}
+     
+    for key, value in phone_numbers.items():
+        print(f"{key} has as phone number {value}")
