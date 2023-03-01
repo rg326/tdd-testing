c91e13561ac745a1b69bea5f2afcc0bb6daec372
Lauren <rg326@live.com>
2023-02-28 22:25:42 -0500

    dict loop exercise sec7, a_s

--- ardit_sulce/mega_course/sec7_loops.py
+++ ardit_sulce/mega_course/sec7_loops.py
@@ -83,3 +83,9 @@ phone_numbers = {"John": "+37682929928", "Marry": "+423998200919"}
      
     for key, value in phone_numbers.items():
         print(f"{key} has as phone number {value}")
+
+# Ex - Dictionary Loop and String Formatting
+phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
+
+for key, value in phone_numbers.items():
+    print(f"{key}: {value}")
