0cd5fbf74b5309f607903d6324182766d5966a9c
Lauren <rg326@live.com>
2023-03-02 21:42:18 -0500

    added sick day, a_s

--- ardit_sulce/mega_course/sec7_loops.py
+++ ardit_sulce/mega_course/sec7_loops.py
@@ -96,4 +96,5 @@ phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
 for value in phone_numbers.values():
     print(value.replace("+", "00"))
 
-#sick day 2
+#sick day 
+#sick day
