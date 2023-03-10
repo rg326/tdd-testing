634fb5369eefe8ba3ae6a8dcb7cf11cdc5f155b9
Lauren <rg326@live.com>
2023-03-09 22:37:05 -0500

    added date-time notes while sec7, a_s

--- ardit_sulce/mega_course/sec7_while.py
+++ ardit_sulce/mega_course/sec7_while.py
@@ -23,3 +23,9 @@ while True:
         continue
 
 # Date and Time
+#We also have while-loops. The code under a while-loop will run as long as the while-loop condition is true:
+
+    while datetime.datetime.now() < datetime.datetime(2090, 8, 20, 19, 30, 20):
+        print("It's not yet 19:30:20 of 2090.8.20")
+
+#The loop above will print out the string inside print() over and over again until the 20th of August, 2090.
