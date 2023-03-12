30e5a6256442dd9a8b234fbf99ba65bb6521f238
Lauren <rg326@live.com>
2023-03-11 22:59:11 -0500

    added output of first program sec8, a_s

---
+++ ardit_sulce/mega_course/sec8_firstbuild.py
@@ -0,0 +1,5 @@
+# Problem Statement
+# Output - Say something: it's good weather today
+# Output - how is the weather there
+# Say something: /end
+# "It's good weather today. How is the weather there? There's only some clouds here."
