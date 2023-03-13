a22ad2aedc3115825216d371cc16c4103c3d08f6
Lauren <rg326@live.com>
2023-03-12 21:52:27 -0400

    added notes firstbuild sec8, a_s

--- ardit_sulce/mega_course/sec8_firstbuild.py
+++ ardit_sulce/mega_course/sec8_firstbuild.py
@@ -9,3 +9,5 @@
 # Receives strings
 # If the user returns \end, the program recognizes it and returns the output.
 # The program is capitalizing the strings and is adding a period or question mark (conditional)
+# First, think of how to get a raw string and convert it into a sentence.
+# The best way to do it, is by using a function
