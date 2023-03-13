89dba326a7a15e364ef6e9019d47fadf42be784b
Lauren <rg326@live.com>
2023-03-12 21:52:27 -0400

    added notes to firstbuild sec8, a_s

--- ardit_sulce/mega_course/sec8_firstbuild.py
+++ ardit_sulce/mega_course/sec8_firstbuild.py
@@ -3,3 +3,9 @@
 # Output - how is the weather there
 # Say something: /end
 # "It's good weather today. How is the weather there? There's only some clouds here."
+
+# APPROACH
+# Program asks for user input repeatedly
+# Receives strings
+# If the user returns \end, the program recognizes it and returns the output.
+# The program is capitalizing the strings and is adding a period or question mark (conditional)
