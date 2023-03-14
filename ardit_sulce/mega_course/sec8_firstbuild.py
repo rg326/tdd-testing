d51fa6850b7d88d7665215157119f946ad059dd8
Lauren <rg326@live.com>
2023-03-13 23:04:11 -0400

    added program firstbuild sec8, a_s

--- ardit_sulce/mega_course/sec8_firstbuild.py
+++ ardit_sulce/mega_course/sec8_firstbuild.py
@@ -11,3 +11,13 @@
 # The program is capitalizing the strings and is adding a period or question mark (conditional)
 # First, think of how to get a raw string and convert it into a sentence.
 # The best way to do it, is by using a function
+
+def sentence_maker(phrase):
+    capitalized = phrase.capitalize()
+    interrogatives = ("who","what","when","where","why","how","do","does","did")
+    if phrase.startswith(interrogatives):
+        return "{}?".format(capitalized)
+    else:
+        return "{}."format(capitalized)
+
+print(sentence_maker("how are you"))
