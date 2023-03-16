5cc1b98e3a30c970298620636a93e2ccc4f8ced1
Lauren <rg326@live.com>
2023-03-15 22:50:18 -0400

    added list comp with if conditional sec9, a_s

--- ardit_sulce/mega_course/sec9_listcomp.py
+++ ardit_sulce/mega_course/sec9_listcomp.py
@@ -13,3 +13,10 @@ print(new_temps)
 new_temps = [temp / 10 for temp in temps]
 
 print(new_temps)
+
+# List Comp with Conditionals
+temps = [221, 234, 340, -9999, 230]
+
+new_temps = [temp / 10 for temp iin temps if temp != -9999]
+
+print(new_temps)
