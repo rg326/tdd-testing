94806581dab80b506e2901b150e95cd3de7d0575
Lauren <rg326@live.com>
2023-03-15 22:50:18 -0400

    added example of list comprehension listcomp sec9, a_s

--- ardit_sulce/mega_course/sec9_listcomp.py
+++ ardit_sulce/mega_course/sec9_listcomp.py
@@ -1,1 +1,15 @@
 #Intro to List Comprehensions
+temps = [221, 234, 340, 230]
+
+# Not using decimals/floats saves space on servers
+
+new_temps = []
+for temp in temps:
+    new_temps.append(temp / 10)
+
+print(new_temps)
+
+# A neater way to do this: list comprehension
+new_temps = [temp / 10 for temp in temps]
+
+print(new_temps)
