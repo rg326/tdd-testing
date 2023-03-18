b255ca3ef94711ab42a93a8fac117cc9499e9601
Lauren <rg326@live.com>
2023-03-17 21:19:54 -0400

    added list comp for if-else sec9, a_s

--- ardit_sulce/mega_course/sec9_listcomp.py
+++ ardit_sulce/mega_course/sec9_listcomp.py
@@ -28,3 +28,11 @@ def foo(lst):
 # Ex - Only Positive Numbers
 def foo(lst):
     return [i for i in lst if i > 0 ]
+
+
+# List Comp with If-Else
+temps = [221, 234, 340, -9999, 230]
+
+new_temps = [temp / 10 if temp != -9999 else 0 for temp in temps]
+
+print[new_temps]
