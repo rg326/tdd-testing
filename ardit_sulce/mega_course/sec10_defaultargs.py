a90e311f8c92b2e23c7d6ed051bb3e8b5be6304b
Lauren <rg326@live.com>
2023-03-24 22:09:06 -0400

    added default args sec10, a_s

---
+++ ardit_sulce/mega_course/sec10_defaultargs.py
@@ -0,0 +1,11 @@
+# Keyword Arguments
+# Nonkeyword/Positional Arguments
+
+def area(a, b=4): # Default Param
+    return a* b
+
+print(area(b=4, a=5)) #Positional
+print(area(a=5, b=7)):
+print(area(5, 7))
+
+# Remember, a default param CANNOT come before a non-default param)
