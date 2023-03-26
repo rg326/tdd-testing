cb3806f97eb3f23d3a8231b3a14707f85f742cdc
Lauren <rg326@live.com>
2023-03-25 23:27:18 -0400

    added arbitrary non-keyword arguments sec10, a_s

--- ardit_sulce/mega_course/sec10_defaultargs.py
+++ ardit_sulce/mega_course/sec10_defaultargs.py
@@ -10,3 +10,12 @@ print(area(5, 7))
 
 # Remember, a default param CANNOT come before a non-default param)
 
+#Functions witg an Arbitrary Number of Non-Keyword Arguments
+def mean(*args):
+    return sum(args)/len(args)
+
+print(mean(1,3,4))
+#you get a tuple that contains all the args in the parameter
+
+
+
